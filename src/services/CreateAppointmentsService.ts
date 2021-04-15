523fb529333df8ec6a9c966486a0b786d15579a8
REBECANONATO <rebecanonato89@gmail.com>
2021-04-15 14:25:28 -0300

    Create User Routes and Services

--- src/services/CreateAppointmentsService.ts
+++ src/services/CreateAppointmentsService.ts
@@ -5,7 +5,7 @@ import Appointment from '../models/Appointment';
 
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
@@ -23,7 +23,7 @@ interface Request {
 
 class CreateAppointmentService {
 
-  public async execute({ date, provider }: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date); // regra de negócio
@@ -37,7 +37,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate
     });
 
