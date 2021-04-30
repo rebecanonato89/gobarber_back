ce343fd3529f37faceb3715e669c0b6061d67861
REBECANONATO <rebecanonato89@gmail.com>
2021-04-30 16:47:24 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes/routes';
 import uploadConfig from './config/upload';
+import cors from 'cors';
 
 
 import './database';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
